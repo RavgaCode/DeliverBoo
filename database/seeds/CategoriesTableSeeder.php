af237d49eb1d826b47b299ee4d7ba35db7d858d1
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-09-27 10:16:35 +0200

    Categories table, model, seeder

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Category;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories=[
+            'Pizzeria',
+            'Sushi',
+            'Fast Food',
+            'Asian',
+            'Vegetariano',
+            'Italiano',
+        ];
+        // Per ogni elemento dell'array creo una nuova riga nel db
+        foreach($categories as $category_name){
+            $new_category = new Category();
+            $new_category->name = $category_name;
+            $new_category->save();
+        }
+    }
+}
